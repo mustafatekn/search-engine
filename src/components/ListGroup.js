ad308eb342648e16bcf3ab3457174efd5ef3e576
Mustafa <mustafateknn@gmail.com>
2022-01-01 16:18:51 +0300

    sorting by name and year

--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,14 +1,59 @@
 import React from "react";
+import { useSearchState, useSearchDispatch } from "../context/search";
 
-export default function ListGroup({setIsVisible}) {
+export default function ListGroup({ setIsVisible }) {
+  const searchState = useSearchState();
+  let searchResults = searchState && searchState.searchResults;
+
+  const dispatch = useSearchDispatch();
+
+  const orderByName = () => {
+    searchResults = searchResults.sort(nameComparator);
+    dispatch({ type: "SET_SEARCH_RESULTS", payload: searchResults });
+  };
+
+  const orderByNameDesc = () => {
+    searchResults = searchResults.sort(nameComparator).reverse();
+    dispatch({ type: "SET_SEARCH_RESULTS", payload: searchResults });
+  };
+    
+  const orderByYear = () => {
+    searchResults = searchResults.sort(yearComparator);
+    dispatch({ type: "SET_SEARCH_RESULTS", payload: searchResults });
+  };
+
+  const orderByYearDesc = () => {
+    searchResults = searchResults.sort(yearComparator).reverse();
+    dispatch({ type: "SET_SEARCH_RESULTS", payload: searchResults });
+  };
+
+  const nameComparator = (a, b) => {
+    if (a[0] < b[0]) return -1;
+    if (a[0] > b[0]) return 1;
+    return 0;
+  };
+
+  const yearComparator = (a, b) => {
+    if (a[3].split('/')[2] < b[3].split('/')[2]) return -1;
+    if (a[3].split('/')[2] > b[3].split('/')[2]) return 1;
+    return 0;
+  };
 
   return (
     <div id="listGroupWrapper">
       <ul className="listGroup" onMouseLeave={() => setIsVisible(false)}>
-        <li className="listGroupItem">Name ascending</li>
-        <li className="listGroupItem">Name descending</li>
-        <li className="listGroupItem">Year ascending</li>
-        <li className="listGroupItem">Year descending</li>
+        <li className="listGroupItem" onClick={orderByName}>
+          Name ascending
+        </li>
+        <li className="listGroupItem" onClick={orderByNameDesc}>
+          Name descending
+        </li>
+        <li className="listGroupItem" onClick={orderByYear}>
+          Year ascending
+        </li>
+        <li className="listGroupItem" onClick={orderByYearDesc}>
+          Year descending
+        </li>
       </ul>
     </div>
   );
